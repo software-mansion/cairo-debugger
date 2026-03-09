8df019b8fbd60179f207278d576f521da4fd9b4e
Piotr Magiera <56825108+piotmag769@users.noreply.github.com>
2026-03-09 10:46:18 +0100

    Add variables to ui state (#87)
    * Separate post and pre step update of call stack
    
    commit-id:9b16fd1f
    
    * Add variables to ui state
    
    commit-id:2e4df8a0

--- src/debugger/state.rs
+++ src/debugger/state.rs
@@ -17,6 +17,8 @@ type SourcePath = String;
 mod call_stack;
 mod ui_state;
 
+pub use call_stack::RequestedVariables;
+
 pub struct State {
     configuration_done: bool,
     execution_stopped: bool,
